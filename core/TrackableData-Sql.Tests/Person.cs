ac7f2666e9492210c9bd9d7932f9e59fef96cc1b
Esun Kim <veblush@gmail.com>
2015-10-14 14:49:25 +0900

    Add TrackableData-Sql.Tests

---
+++ core/TrackableData-Sql.Tests/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackableData.Sql.Tests
+{
+    public interface IPerson : ITrackablePoco
+    {
+        string Name { get; set; }
+        int Age { get; set; }
+        TrackableHand LeftHand { get; set; }
+        TrackableHand RightHand { get; set; }
+    }
+
+    public interface IHand : ITrackablePoco
+    {
+        TrackableRing MainRing { get; set; }
+        TrackableRing SubRing { get; set; }
+    }
+
+    public interface IRing : ITrackablePoco
+    {
+        string Name { get; set; }
+        int Power { get; set; }
+    }
+}
