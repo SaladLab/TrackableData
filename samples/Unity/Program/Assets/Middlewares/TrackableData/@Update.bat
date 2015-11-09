8c5ebacb4dd16fb35dc5bd7b8d32d47317bbbc47
Esun Kim <veblush@gmail.com>
2015-11-09 16:06:18 +0900

    Cleanup build script / project structure

---
+++ samples/Unity/Program/Assets/Middlewares/TrackableData/@Update.bat
@@ -0,0 +1,4 @@
+COPY /Y ..\..\..\..\..\..\core\TrackableData.Net35\bin\Release\TrackableData.dll .
+COPY /Y ..\..\..\..\..\..\plugins\TrackableData-Json.Net35\bin\Release\TrackableData-Json.dll .
+COPY /Y ..\..\..\..\..\..\plugins\TrackableData-Protobuf.Net35\bin\Release\TrackableData-Protobuf.dll .
+pause.
