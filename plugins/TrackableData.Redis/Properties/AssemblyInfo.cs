6084d87360ca07d8c199ff1eb63b67c48123a246
Esun Kim <veblush@gmail.com>
2016-03-17 21:19:08 +0900

    Add Redis plugins

---
+++ plugins/TrackableData.Redis/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TrackableData.Redis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SaladLab")]
+[assembly: AssemblyProduct("TrackableData.Redis")]
+[assembly: AssemblyCopyright("Copyright © 2016 SaladLab")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("df4668db-46c9-4fdc-afc2-418deba27d12")]
