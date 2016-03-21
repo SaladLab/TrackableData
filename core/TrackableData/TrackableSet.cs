08fe68270dca6b34956f2de6a1dc538715f7b931
Esun Kim <veblush@gmail.com>
2016-03-22 00:05:55 +0900

    Add TrackableSet

---
+++ core/TrackableData/TrackableSet.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TrackableData
+{
+    public class TrackableSet<T> :
+#if !NET35
+        ISet<T>,
+#endif
+        ICollection<T>, ITrackable<ICollection<T>>
+    {
+        private readonly HashSet<T> _set = new HashSet<T>();
+
+        // Specific tracker
+
+        public ISetTracker<T> Tracker { get; set; }
+
+        // ITrackable
+
+        public bool Changed
+        {
+            get { return Tracker != null && Tracker.HasChange; }
+        }
+
+        ITracker ITrackable.Tracker
+        {
+            get { return Tracker; }
+
+            set
+            {
+                var tracker = (ISetTracker<T>)value;
+                Tracker = tracker;
+            }
+        }
+
+        ITracker<ICollection<T>> ITrackable<ICollection<T>>.Tracker
+        {
+            get { return Tracker; }
+
+            set
+            {
+                var tracker = (ISetTracker<T>)value;
+                Tracker = tracker;
+            }
+        }
+
+        public ITrackable GetChildTrackable(object name)
+        {
+            return null;
+        }
+
+        public IEnumerable<KeyValuePair<object, ITrackable>> GetChildTrackables(bool changedOnly = false)
+        {
+            yield break;
+        }
+
+        // ISet<T>
+
+        public bool Add(T item)
+        {
+            if (_set.Add(item))
+            {
+                if (Tracker != null)
+                    Tracker.TrackAdd(item);
+                return true;
+            }
+            return false;
+        }
+
+        public void UnionWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            return _set.IsSubsetOf(other);
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            return _set.IsSupersetOf(other);
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            return _set.IsProperSupersetOf(other);
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            return _set.IsProperSubsetOf(other);
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            return _set.Overlaps(other);
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            return _set.SetEquals(other);
+        }
+
+        // ICollection<T>
+
+        void ICollection<T>.Add(T item)
+        {
+            if (_set.Add(item))
+            {
+                if (Tracker != null)
+                    Tracker.TrackAdd(item);
+            }
+        }
+
+        public void Clear()
+        {
+            if (Tracker != null)
+            {
+                foreach (var i in _set)
+                    Tracker.TrackRemove(i);
+            }
+
+            _set.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _set.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            ((ICollection<T>)_set).CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            if (_set.Remove(item))
+            {
+                if (Tracker != null)
+                    Tracker.TrackRemove(item);
+                return true;
+            }
+            return false;
+        }
+
+        public int Count
+        {
+            get { return _set.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        // IEnumerator<T>
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _set.GetEnumerator();
+        }
+
+        // IEnumerable
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable)_set).GetEnumerator();
+        }
+    }
+}
---
+++ core/TrackableData/TrackableSetTracker.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TrackableData
+{
+    public enum TrackableSetOperation : byte
+    {
+        None = 0,
+        Add = 1,
+        Remove = 2,
+    }
+
+    public class TrackableSetTracker<T> : ISetTracker<T>
+    {
+        public Dictionary<T, TrackableSetOperation> ChangeMap = new Dictionary<T, TrackableSetOperation>();
+
+        public bool GetChange(T value, out TrackableSetOperation operation)
+        {
+            return ChangeMap.TryGetValue(value, out operation);
+        }
+
+        public void TrackAdd(T value)
+        {
+            TrackableSetOperation prevOperation;
+            GetChange(value, out prevOperation);
+
+            switch (prevOperation)
+            {
+                case TrackableSetOperation.None:
+                    SetChange(value, TrackableSetOperation.Add);
+                    break;
+
+                case TrackableSetOperation.Add:
+                    throw new InvalidOperationException("Add after add is impossbile.");
+
+                case TrackableSetOperation.Remove:
+                    ChangeMap.Remove(value);
+                    break;
+            }
+        }
+
+        public void TrackRemove(T value)
+        {
+            TrackableSetOperation prevOperation;
+            GetChange(value, out prevOperation);
+
+            switch (prevOperation)
+            {
+                case TrackableSetOperation.None:
+                    SetChange(value, TrackableSetOperation.Remove);
+                    break;
+
+                case TrackableSetOperation.Add:
+                    ChangeMap.Remove(value);
+                    break;
+
+                case TrackableSetOperation.Remove:
+                    throw new InvalidOperationException("Remove after remove is impossbile.");
+            }
+        }
+
+        private void SetChange(T value, TrackableSetOperation operation)
+        {
+            var hasChangedBefore = HasChange;
+
+            ChangeMap[value] = operation;
+
+            if (HasChangeSet != null && hasChangedBefore == false)
+                HasChangeSet(this);
+        }
+
+        public IEnumerable<T> AddValues
+        {
+            get
+            {
+                return ChangeMap.Where(i => i.Value == TrackableSetOperation.Add)
+                                .Select(i => i.Key);
+            }
+        }
+
+        public IEnumerable<T> RemoveValues
+        {
+            get
+            {
+                return ChangeMap.Where(i => i.Value == TrackableSetOperation.Remove)
+                                .Select(i => i.Key);
+            }
+        }
+
+        // ITracker
+
+        public bool HasChange
+        {
+            get { return ChangeMap.Any(); }
+        }
+
+        public event TrackerHasChangeSet HasChangeSet;
+
+        public void Clear()
+        {
+            ChangeMap.Clear();
+        }
+
+        public void ApplyTo(object trackable)
+        {
+            ApplyTo((ICollection<T>)trackable);
+        }
+
+        public void ApplyTo(ICollection<T> trackable)
+        {
+            if (trackable == null)
+                throw new ArgumentNullException("trackable");
+
+            foreach (var item in ChangeMap)
+            {
+                switch (item.Value)
+                {
+                    case TrackableSetOperation.Add:
+                        trackable.Add(item.Key);
+                        break;
+
+                    case TrackableSetOperation.Remove:
+                        trackable.Remove(item.Key);
+                        break;
+                }
+            }
+        }
+
+        public void ApplyTo(ITracker tracker)
+        {
+            ApplyTo((TrackableSetTracker<T>)tracker);
+        }
+
+        public void ApplyTo(ITracker<ICollection<T>> tracker)
+        {
+            ApplyTo((TrackableSetTracker<T>)tracker);
+        }
+
+        public void ApplyTo(TrackableSetTracker<T> tracker)
+        {
+            if (tracker == null)
+                throw new ArgumentNullException("tracker");
+
+            foreach (var item in ChangeMap)
+            {
+                switch (item.Value)
+                {
+                    case TrackableSetOperation.Add:
+                        tracker.TrackAdd(item.Key);
+                        break;
+
+                    case TrackableSetOperation.Remove:
+                        tracker.TrackRemove(item.Key);
+                        break;
+                }
+            }
+        }
+
+        public void RollbackTo(object trackable)
+        {
+            RollbackTo((ICollection<T>)trackable);
+        }
+
+        public void RollbackTo(ICollection<T> trackable)
+        {
+            if (trackable == null)
+                throw new ArgumentNullException("trackable");
+
+            foreach (var item in ChangeMap)
+            {
+                switch (item.Value)
+                {
+                    case TrackableSetOperation.Add:
+                        trackable.Remove(item.Key);
+                        break;
+
+                    case TrackableSetOperation.Remove:
+                        trackable.Add(item.Key);
+                        break;
+                }
+            }
+        }
+
+        public void RollbackTo(ITracker tracker)
+        {
+            RollbackTo((TrackableSetTracker<T>)tracker);
+        }
+
+        public void RollbackTo(ITracker<ICollection<T>> tracker)
+        {
+            RollbackTo((TrackableSetTracker<T>)tracker);
+        }
+
+        public void RollbackTo(TrackableSetTracker<T> tracker)
+        {
+            if (tracker == null)
+                throw new ArgumentNullException("tracker");
+
+            foreach (var item in ChangeMap)
+            {
+                switch (item.Value)
+                {
+                    case TrackableSetOperation.Add:
+                        tracker.TrackRemove(item.Key);
+                        break;
+
+                    case TrackableSetOperation.Remove:
+                        tracker.TrackAdd(item.Key);
+                        break;
+                }
+            }
+        }
+
+        // Object
+
+        public override string ToString()
+        {
+            return "{ " + string.Join(", ", ChangeMap.Select(x =>
+            {
+                switch (x.Value)
+                {
+                    case TrackableSetOperation.Add:
+                        return "+" + x.Key;
+
+                    case TrackableSetOperation.Remove:
+                        return "-" + x.Key;
+
+                    default:
+                        return "";
+                }
+            }).ToArray()) + " }";
+        }
+    }
+}
