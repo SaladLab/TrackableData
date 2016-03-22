0296a6f6c2283312b1d05358d3568cfb91939514
Esun Kim <veblush@gmail.com>
2016-03-22 11:20:47 +0900

    Add TrackableSet to Protobuf

---
+++ plugins/TrackableData.Protobuf.Tests/TrackableSetTest.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using ProtoBuf.Meta;
+using Xunit;
+
+namespace TrackableData.Protobuf.Tests
+{
+    public class TrackableSetTest
+    {
+        private TrackableSet<int> CreateTestSet()
+        {
+            return new TrackableSet<int>() { 1, 2, 3 };
+        }
+
+        private TrackableSet<int> CreateTestSetWithTracker()
+        {
+            var set = CreateTestSet();
+            set.SetDefaultTrackerDeep();
+            return set;
+        }
+
+        private TypeModel CreateTypeModel()
+        {
+            var model = TypeModel.Create();
+            model.Add(typeof(TrackableSetTracker<int>), false)
+                 .SetSurrogate(typeof(TrackableSetTrackerSurrogate<int>));
+            return model;
+        }
+
+        [Fact]
+        public void Test_TrackableSet_Serialize_Work()
+        {
+            var set = CreateTestSetWithTracker();
+
+            var typeModel = CreateTypeModel();
+            var set2 = (IEnumerable<int>)typeModel.DeepClone(set);
+
+            Assert.Equal(new HashSet<int> { 1, 2, 3 }, set2);
+        }
+
+        [Fact]
+        public void Test_TrackableSetTracker_Serialize_Work()
+        {
+            var set = CreateTestSetWithTracker();
+            set.Remove(1);
+            set.Remove(2);
+            set.Add(4);
+            set.Add(5);
+
+            var typeModel = CreateTypeModel();
+            var tracker2 = (TrackableSetTracker<int>)typeModel.DeepClone(set.Tracker);
+
+            var set2 = CreateTestSet();
+            tracker2.ApplyTo(set2);
+
+            Assert.Equal(new HashSet<int> { 3, 4, 5 }, set2);
+        }
+    }
+}
---
+++ plugins/TrackableData.Protobuf/TrackableSetTrackerSurrogate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using ProtoBuf;
+
+namespace TrackableData.Protobuf
+{
+    [ProtoContract]
+    public class TrackableSetTrackerSurrogate<T>
+    {
+        [ProtoMember(1)] public List<T> AddValues = new List<T>();
+        [ProtoMember(2)] public List<T> RemoveValues = new List<T>();
+
+        public static implicit operator TrackableSetTrackerSurrogate<T>(TrackableSetTracker<T> tracker)
+        {
+            if (tracker == null)
+                return null;
+
+            var surrogate = new TrackableSetTrackerSurrogate<T>();
+            foreach (var change in tracker.ChangeMap)
+            {
+                if (change.Value == TrackableSetOperation.Add)
+                    surrogate.AddValues.Add(change.Key);
+                else
+                    surrogate.RemoveValues.Add(change.Key);
+            }
+            return surrogate;
+        }
+
+        public static implicit operator TrackableSetTracker<T>(TrackableSetTrackerSurrogate<T> surrogate)
+        {
+            if (surrogate == null)
+                return null;
+
+            var tracker = new TrackableSetTracker<T>();
+            foreach (var value in surrogate.AddValues)
+                tracker.TrackAdd(value);
+            foreach (var value in surrogate.RemoveValues)
+                tracker.TrackRemove(value);
+            return tracker;
+        }
+    }
+}
